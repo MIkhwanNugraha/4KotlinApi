87dcb6e67dd7084d5ab30f5be48d47ad66c3ea70
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-12-23 13:25:56 +0400

    2

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/Presenter/FirstPresenter.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/Presenter/FirstPresenter.kt
@@ -1,4 +1,25 @@
 package com.group.nugraha.matchschedulekotlin.Presenter
 
-class FirstPresenter {
+import com.google.gson.Gson
+import com.group.nugraha.matchschedulekotlin.View.FirstView
+import com.group.nugraha.matchschedulekotlin.api.ApiRepository
+import com.group.nugraha.matchschedulekotlin.api.TheSportDBApi
+import com.group.nugraha.matchschedulekotlin.model.ResultTeamResponse
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class FirstPresenter (private val resultview: FirstView,
+                      private val apiRepository: ApiRepository,
+                      private val gson: Gson) {
+    fun getResultTeamList(){ //di sini biasanya diandak (league: String?)
+        doAsync {
+            val resultdata = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getResultTeams()), //disini diandak (league)
+                ResultTeamResponse::class.java)
+
+            uiThread {
+                resultview.showResultTeamList(resultdata.resultteams)
+            }
+        }
+    }
 }
