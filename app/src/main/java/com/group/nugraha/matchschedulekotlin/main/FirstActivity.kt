799c098e948bb8c44229d80bec4f546399daf942
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-12-23 13:36:05 +0400

    2

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/main/FirstActivity.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/main/FirstActivity.kt
@@ -2,11 +2,32 @@ package com.group.nugraha.matchschedulekotlin.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class FirstActivity : AppCompatActivity() {
 
+    private lateinit var resultlistTeam: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        linearLayout {
+            lparams(matchParent, wrapContent)
+            orientation = LinearLayout.VERTICAL
+            padding = dip(15) ///kena coba pindah paddingnya 30
+
+            resultlistTeam = recyclerView {
+                lparams(matchParent, wrapContent)
+                layoutManager = LinearLayoutManager(context)
+            }
+//sampai meulah adapter...23 des 01.35 pm
+        }
+
+
 
 
     }
