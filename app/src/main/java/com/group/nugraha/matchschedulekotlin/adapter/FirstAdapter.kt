c5691174c403f92a2086eed77aff9113780e3ff9
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-12-23 15:06:39 +0400

    2

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/adapter/FirstAdapter.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/adapter/FirstAdapter.kt
@@ -4,20 +4,23 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
+import com.group.nugraha.matchschedulekotlin.R
+import com.group.nugraha.matchschedulekotlin.R.id.team_event
 import com.group.nugraha.matchschedulekotlin.model.ResultTeam
 import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class FirstAdapter (private val resultteams: List<ResultTeam>, private val lastlistener: (ResultTeam) -> Unit)  //ini awal muncul listener, last itu sama kaya result, kasan ke detailnya
     : RecyclerView.Adapter<ResultTeamViewHolder>(){
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ResultTeamViewHolder {
-   }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ResultTeamViewHolder(LastTeamUI().createView(AnkoContext.create(p0.context, p0)))
     }
 
+    override fun getItemCount(): Int = resultteams.size
+
     override fun onBindViewHolder(p0: ResultTeamViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        p0.bindItem(resultteams[p1], lastlistener)
     }
 
 }
@@ -27,14 +30,31 @@ class LastTeamUI : AnkoComponent<ViewGroup>{
         return with(ui){
             linearLayout {
                 lparams(matchParent, wrapContent)
-                padding
+                padding = dip(5)
+                orientation = LinearLayout.VERTICAL
+
+                textView {
+                    id = R.id.team_event
+                    textSize = 16f
+                }.lparams{
+                    margin = dip(15)
+                }
+
+                //skor home dan away balum ditambah
             }
         }
     }
 }
 
 class ResultTeamViewHolder (lastview: View): RecyclerView.ViewHolder(lastview){
-    fun bindItem(lastteams: ResultTeam, lastlistener: (ResultTeam) -> Unit){
+    private val yangbertanding: TextView = lastview.find(team_event)
 
+
+    fun bindItem(lastteams: ResultTeam, lastlistener: (ResultTeam) -> Unit){
+        yangbertanding.text = lastteams.teamIdevent
+
+        yangbertanding.onClick {
+            lastlistener(lastteams)
+        }
     }
 }
--- app/src/main/java/com/group/nugraha/matchschedulekotlin/main/FirstActivity.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/main/FirstActivity.kt
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
+import com.google.gson.Gson
+import com.group.nugraha.matchschedulekotlin.Presenter.FirstPresenter
+import com.group.nugraha.matchschedulekotlin.View.FirstView
+import com.group.nugraha.matchschedulekotlin.adapter.FirstAdapter
+import com.group.nugraha.matchschedulekotlin.api.ApiRepository
+import com.group.nugraha.matchschedulekotlin.model.ResultTeam
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 
-class FirstActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity(), FirstView {
 
     private lateinit var resultlistTeam: RecyclerView
+    private lateinit var presenterik: FirstPresenter
+    private lateinit var adapterik: FirstAdapter
+    private var resultteams: MutableList<ResultTeam> = mutableListOf() //ResultTeamnya dapat dari model
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +35,19 @@ class FirstActivity : AppCompatActivity() {
             }
         }
 
+        adapterik = FirstAdapter(resultteams){
 
+        }
 
+        resultlistTeam.adapter = adapterik
+        presenterik = FirstPresenter(this, ApiRepository(), Gson())
+
+    }
+
+    override fun showResultTeamList(data: List<ResultTeam>) {
+        resultteams.clear()
+        resultteams.addAll(data)
+        adapterik.notifyDataSetChanged()
 
     }
 }
